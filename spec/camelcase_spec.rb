696223ae49fdc31e7f1867eca46e204f84436a19
Luke Donovan <lukelamnisos@icloud.com>
2020-02-13 15:48:11 +0000

    added more tests to camelcase.spec

--- spec/camelcase_spec.rb
+++ spec/camelcase_spec.rb
@@ -1,11 +1,15 @@
 require 'camelcase'
 
 describe "camelcase" do
-  it "returns 'camel case' when given 'camelCase'" do
+  it "returns 'camel Case' when given 'camelCase'" do
   expect(camelCase('camelCase')).to eq 'camel Case'
   end
 
-  it "returns 'luke donovan' when given 'lukeDonovan'" do
+  it "returns 'luke Donovan' when given 'lukeDonovan'" do
     expect(camelCase('lukeDonovan')).to eq 'luke Donovan'
+  end
+
+  it "returns 'hello There Friend' when given 'helloThereFriend'" do
+    expect(camelCase(helloThereFriend)).to eq 'hello There Friend'
   end
 end  
